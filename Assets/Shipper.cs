673a8e4880f7ff4eaedec05045108ed1032f972a
David <david.ohldin@elev.ga.lbs.se>
2018-10-10 15:11:47 +0200

    Examination 2

--- Assets/Shipper.cs
+++ Assets/Shipper.cs
@@ -16,7 +16,7 @@ public class Shipper : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        //Random snabbhet och rotation
+        //Random snabbhet, rotation och start-position
         movespeed += Random.Range(0, 5);
         rotate += Random.Range(10, 30);
         transform.position = new Vector3(Random.Range(-18, 18), Random.Range(-10, 10));
@@ -25,26 +25,30 @@ public class Shipper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        //Counter ökar med tid och ökar timer med ett ifall counter >= timer + 1 och printar värdet.  
+        //En float ökar med tiden och ökar en int med ett ifall counter >= timer + 1 och printar värdet.  
         counter += Time.deltaTime;
         if (counter >= timer + 1)
         {
             timer += 1;
             print("timer" + ' ' + timer);
         }
-
+        //att den alltid åker framåt utan att den stoppas av fps
         transform.Translate(movespeed * Time.deltaTime, 0, 0);
+        //vänster/höger/broms
         if (Input.GetKey(KeyCode.A))
         {
+            //byter färg och roterar vänster oberoende av fps
             rend.color = new Color(0, 1, 0);
             transform.Rotate(0, 0, rotate * Time.deltaTime * 10);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
+            //byter färg och roterar höger(dubbelt så snabbt) oberoende av fps
             rend.color = new Color(0, 0, 1);
             transform.Rotate(0, 0, -rotate * Time.deltaTime * 20);
         }
+        //vid nedtryckning halveras snabbheten(broms), vid släpp dubblas den(tillbaka till vanligt) 
         if (Input.GetKeyDown(KeyCode.S))
          {
             movespeed = movespeed / 2;
@@ -53,13 +57,19 @@ public class Shipper : MonoBehaviour
         {
             movespeed = movespeed * 2;
         }
+        //random färg(gjorde bara GetKey istället för GetKeyDown för att det var roligare(annars hade den bara bytt en gång vid nedtryckning))
         if (Input.GetKey(KeyCode.Space))
         {
+            // tre floats får ett random värde mellan 0 och 1
             Färg1 = Random.Range(0f, 1f);
             Färg2 = Random.Range(0f, 1f);
             Färg3 = Random.Range(0f, 1f);
+            //dem tre floatsen blir ingredienserna till den nya färgen
             rend.color = new Color(Färg1, Färg2, Färg3);
         }
+        //warping
+        //checkar ifall om objektet har en x-position utanför 19 och -19 och om y är utanför 11 och -11
+        //ifall om objekten är utanför så flyttas den till motsata x/y-kant och behåller sin y/x position
         if (transform.position.x < -19)
         {
             transform.position = new Vector3 (19, transform.position.y);
